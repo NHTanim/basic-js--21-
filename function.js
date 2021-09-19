f6d61048c79de0756aec2d81b599df214050785b
NHTanim <nazmulhossaintanim78@gmail.com>
2021-09-20 00:50:26 +0600

    js basic

---
+++ function.js
@@ -0,0 +1,18 @@
+// 
+
+
+
+
+
+function getFullName (firstname, lastname){
+  const fullName = firstname + " " +lastname;
+  return fullName;
+}
+
+const name = getFullName ('Hanif', 'Poribohon');
+console.log(name);
+
+
+
+
+// 
